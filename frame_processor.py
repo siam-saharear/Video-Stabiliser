55f21ad832f18042ba5271337eecec2684526800
jeher <siamsaharear010@gmail.com>
2026-03-05 03:43:13 +0600

    add 'frame_resizer' function

--- frame_processor.py
+++ frame_processor.py
@@ -46,3 +46,10 @@ def load_video(video_path):
 
 
 
+def frame_resizer(frame, max_height=1000, max_width=None): 
+    if max_width:
+        resized_frame = imutils.resize(frame, width=max_width) 
+    else: 
+        resized_frame = imutils.resize(frame, height=max_height) 
+    return resized_frame
+
